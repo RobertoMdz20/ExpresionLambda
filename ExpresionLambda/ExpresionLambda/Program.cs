e20165374b7cce9ea8a6c1bb5561424b3195c304
ROBERTO ALEJANDRO MENDOZA VELEZ <e1312856816@live.uleam.edu.ec>
2022-01-14 21:16:13 -0500

    Solución ejemplo de planteamiento de expresiones lambda

---
+++ ExpresionLambda/ExpresionLambda/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpresionLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creación de lista.
+            List<int> datos = new List<int>();
+            datos.AddRange(new int[] { 93, 32, 44, 87, 20, 64, 43, 75, 28, 69, 4, 17 });
+
+            //Expresión lambda.
+            List<int> pares = datos.FindAll(num => (num % 2) == 0);
+
+            //Texto.
+            Console.WriteLine("Los números pares del listado son:");
+
+            //Mediante le foreach realizamos la iteración de los números pares.
+            foreach (int numpares in pares)
+
+                //Mostramos por pantalla los datos.
+                Console.WriteLine(numpares);
+        }
+    }
+}
+
